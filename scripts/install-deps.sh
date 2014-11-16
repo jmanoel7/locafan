c869da838ca47986b975ce71501b44af5a4a1d10
João Manoel <joaomanoel7@gmail.com>
2014-11-16 17:53:51 -0200

    fix correct use on debian-7 and ubuntu-14.04

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,15 +1,26 @@
 #!/bin/sh -e
 sudo apt-get install \
     apache2 \
+    mysql-server \
     mysql-client \
     libmysqlclient-dev \
-    mysql-server \
     php5 \
+    php-pear \
+    php5-gd \
     php5-mysql \
+    php5-imagick \
+    php5-curl \
+    curl \
     phpmyadmin \
+    rsync \
+    cronolog \
+    devscripts \
     build-essential \
     git-core \
     python-dev \
-    python-virtualenv
+    virtualenvwrapper \
+    python-virtualenv \
+    python-pip \
+    python-setuptools
 exit 0
 
